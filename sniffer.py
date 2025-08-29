31f3090bd709073a2f8e6564998163cd16ec9f54
Burak Görez <burakgorez01@gmail.com>
2025-08-29 13:10:35 +0300

    Basics

---
+++ sniffer.py
@@ -0,0 +1,19 @@
+#Basic packet sniffer with python -Burak "paradass" Görez
+
+import scapy.all as scapy
+
+class Sniffer:
+    def get_packet(self,packet:scapy.Packet):
+        if packet.haslayer(scapy.TCP):
+            color = "\033[34m"
+        elif packet.haslayer(scapy.UDP):
+            color = "\033[33m"
+        else:
+            color = "\033[31m"
+        print(f"{color}{packet.summary()}\033[0m")
+    
+    def listen(self):
+        scapy.sniff(prn=self.get_packet)
+
+sniffer = Sniffer()
+sniffer.listen()
